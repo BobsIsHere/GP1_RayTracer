c5e3a8ebf63d542a8ef2956da5717b265fdaaf5b
BobsIsHere <maritte.kindt@student.howest.be>
2023-09-20 16:49:54 +0200

    Include Project

---
+++ source/Renderer.cpp
@@ -0,0 +1,86 @@
+//External includes
+#include "SDL.h"
+#include "SDL_surface.h"
+
+//Project includes
+#include "Renderer.h"
+#include "Math.h"
+#include "Matrix.h"
+#include "Material.h"
+#include "Scene.h"
+#include "Utils.h"
+
+using namespace dae;
+
+Renderer::Renderer(SDL_Window * pWindow) :
+	m_pWindow(pWindow),
+	m_pBuffer(SDL_GetWindowSurface(pWindow))
+{
+	//Initialize
+	SDL_GetWindowSize(pWindow, &m_Width, &m_Height);
+	m_pBufferPixels = static_cast<uint32_t*>(m_pBuffer->pixels);
+}
+
+void Renderer::Render(Scene* pScene) const
+{
+	Camera& camera = pScene->GetCamera();
+	auto& materials = pScene->GetMaterials();
+	auto& lights = pScene->GetLights();
+
+	//for (int px{}; px < m_Width; ++px)
+	//{
+	//	for (int py{}; py < m_Height; ++py)
+	//	{
+	//		float gradient = px / static_cast<float>(m_Width);
+	//		gradient += py / static_cast<float>(m_Width);
+	//		gradient /= 2.0f;
+
+	//		ColorRGB finalColor{ gradient, gradient, gradient };
+
+	//		//Update Color in Buffer
+	//		finalColor.MaxToOne();
+
+	//		m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+	//			static_cast<uint8_t>(finalColor.r * 255),
+	//			static_cast<uint8_t>(finalColor.g * 255),
+	//			static_cast<uint8_t>(finalColor.b * 255));
+	//	}
+	//}
+	int ascpectRatio{ m_Width / m_Height };
+
+	//How work?
+	for (int px{}; px < m_Width; ++px)
+	{
+		for (int py{}; py < m_Height; ++py)
+		{
+			float x{ (2.f * ((static_cast<float>(px) + 0.5f ) / static_cast<float>(m_Width)) - 1.f) * ascpectRatio };
+			float y{ (1.f - 2.f * ((static_cast<float>(py) + 0.5f) / static_cast<float>(m_Height)))};
+
+			Vector3 rayDirection{ x,y, 1.f };
+
+			// lot of clip offs when dont normalize
+			//max value of x, y, z component will be 1 when normalize
+			rayDirection.Normalize();
+
+			Ray hitRay{ {0,0,0}, rayDirection };
+			ColorRGB finalColor{ rayDirection.x, rayDirection.y, rayDirection.z };
+
+			//Update Color in Buffer
+			finalColor.MaxToOne();
+
+			m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+				static_cast<uint8_t>(finalColor.r * 255),
+				static_cast<uint8_t>(finalColor.g * 255),
+				static_cast<uint8_t>(finalColor.b * 255));
+		}
+	}
+
+	//@END
+	//Update SDL Surface
+	SDL_UpdateWindowSurface(m_pWindow);
+}
+
+bool Renderer::SaveBufferToImage() const
+{
+	return SDL_SaveBMP(m_pBuffer, "RayTracing_Buffer.bmp");
+}
